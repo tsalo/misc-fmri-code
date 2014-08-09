e8c2442a596421a0d95af74ffa5bf8e7e78e3dbc
Taylor Salo <tsalo90@gmail.com>
2014-08-09 00:02:48 -0700

    Splitting output files per contrast.

--- save_clusters_and_effect_size.m
+++ save_clusters_and_effect_size.m
@@ -16,23 +16,22 @@ function save_clusters_and_effect_size(spmFile, pThr, corr, k)
 load(spmFile);
 
 design = SPM.xsDes.Design;
-
-conCounter = 1;
-maxClusters = 1;
-
 fprintf(['Evaluating second-level results of design: ' design '.\n']);
 fprintf(['Evaluating at p < ' num2str(pThr) ' ' corr ' and k > ' num2str(k) '.\n']);
 
 for iCon = 1:length(SPM.xCon)
     STAT = SPM.xCon(iCon).STAT;
-    
+
     if strcmp(STAT, 'T')
         spmT = [path '/' SPM.xCon(iCon).Vspm.fname];
-        
         conName = SPM.xCon(iCon).name;
         conName = strrep(conName, ' ', '_');
         fprintf(['\tEvaluating contrast ' num2str(iCon) ', ' conName '\n']);
         outDir = [path '/' num2str(pThr) '_' corr '_clusters/' conName '/'];
+
+        if ~exist(outDir, 'dir')
+            mkdir(outDir);
+        end
 
         % Create Cohen's D image.
         [VspmT, Tvals] = load_nii_spm(spmT);
@@ -41,28 +40,16 @@ for iCon = 1:length(SPM.xCon)
         Dvals = (2 .* Tvals) ./ sqrt(df(2));
         dHeader = VspmT;
         dHeader.fname = [outDir D_name conName '.nii'];
-        
-        if ~exist(outDir, 'dir')
-            mkdir(outDir);
-        end
-        
         save_nii_spm(dHeader, Dvals);
-        
-        outStruct{conCounter}.header{1} = [conName '_X'];
-        outStruct{conCounter+1}.header{1} = [conName '_Y'];
-        outStruct{conCounter+2}.header{1} = [conName '_Z'];
-        outStruct{conCounter+3}.header{1} = [conName '_size'];
-        outStruct{conCounter+4}.header{1} = [conName '_peakT'];
-        outStruct{conCounter+5}.header{1} = [conName '_peakD'];
-        outStruct{conCounter+6}.header{1} = [conName '_meanD'];
-        outStruct{conCounter}.col{1} = '';
-        outStruct{conCounter+1}.col{1} = '';
-        outStruct{conCounter+2}.col{1} = '';
-        outStruct{conCounter+3}.col{1} = '';
-        outStruct{conCounter+4}.col{1} = '';
-        outStruct{conCounter+5}.col{1} = '';
-        outStruct{conCounter+6}.col{1} = '';
-        
+
+        % Set up output csv.
+        outStruct{1}.header{1} = 'x'; outStruct{2}.header{1} = 'y'; outStruct{3}.header{1} = 'z';
+        outStruct{4}.header{1} = 'k'; outStruct{5}.header{1} = 'peak_T'; outStruct{6}.header{1} = 'peak_D';
+        outStruct{7}.header{1} = 'mean_D';
+        outStruct{1}.col{1} = ''; outStruct{2}.col{1} = ''; outStruct{3}.col{1} = '';
+        outStruct{4}.col{1} = ''; outStruct{5}.col{1} = ''; outStruct{6}.col{1} = '';
+        outStruct{7}.col{1} = '';
+
         % Create masks of all clusters and determine mean Cohen's D of each cluster.
         switch corr
             case 'unc'
@@ -72,90 +59,109 @@ for iCon = 1:length(SPM.xCon)
             case 'FDR'
                 tThr  = spm_uc_FDR(pThr, df, STAT, 1, VspmT, 0);
         end
-        
-        outHeader = VspmT;
+
+        clustHeader = VspmT;
         rawData = spm_read_vols(VspmT);
         [allClustMat, nClusters] = spm_bwlabel(double(rawData > tThr), 18);
 
         [clustSize, clustNum] = sort(histc(allClustMat(:), 0:nClusters), 1, 'descend');
         clustSize = clustSize(2:end); clustNum = clustNum(2:end) - 1;
         clustNum = clustNum(clustSize >= k); clustSize = clustSize(clustSize >= k);
-        
+
         for jClust = 1:length(clustSize)
             oneClustMat = zeros(size(allClustMat));
             oneClustMat(allClustMat == clustNum(jClust)) = 1;
-            outHeader.fname = [outDir 'Cluster_' sprintf('%03d', jClust) '.nii'];
-            spm_write_vol(outHeader, oneClustMat);
-            
-            [XYZ ROImat] = roi_find_index(outHeader.fname, 0);
+            clustHeader.fname = [outDir 'Cluster_' sprintf('%03d', jClust) '.nii'];
+            spm_write_vol(clustHeader, oneClustMat);
+
+            [XYZ ROImat] = roi_find_index(clustHeader.fname, 0);
             betaXYZ = adjust_XYZ(XYZ, ROImat, dHeader);
             roi = spm_get_data(dHeader.fname, betaXYZ{1});
             roi(isnan(roi)) = 0;
-            
-            [peakCoord, peakValue, peakMM] = get_peak_d(dHeader.fname, outHeader.fname);
-            
-            outStruct{conCounter}.col{jClust, 1} = peakCoord(1);
-            outStruct{conCounter+1}.col{jClust, 1} = peakCoord(2);
-            outStruct{conCounter+2}.col{jClust, 1} = peakCoord(3);
-            outStruct{conCounter+3}.col{jClust, 1} = clustSize(jClust);
-            outStruct{conCounter+4}.col{jClust, 1} = Tvals(peakMM(1), peakMM(2), peakMM(3));
-            outStruct{conCounter+5}.col{jClust, 1} = peakValue;
-            outStruct{conCounter+6}.col{jClust, 1} = sum(roi(:)) / length(find(roi));
+
+            [peakValue, peakCoord, peakMM] = get_peak_d(dHeader, betaXYZ);
+
+            % Fill in output csv.
+            outStruct{1}.col{jClust, 1} = peakCoord(1);
+            outStruct{2}.col{jClust, 1} = peakCoord(2);
+            outStruct{3}.col{jClust, 1} = peakCoord(3);
+            outStruct{4}.col{jClust, 1} = clustSize(jClust);
+            outStruct{5}.col{jClust, 1} = Tvals(peakMM(1), peakMM(2), peakMM(3));
+            outStruct{6}.col{jClust, 1} = peakValue;
+            outStruct{7}.col{jClust, 1} = sum(roi(:)) / length(find(roi));
         end
-        
-        if length(clustSize) > maxClusters
-            maxClusters = length(clustSize);
-        end
-        
+
         fprintf('\t\t%d out of %d clusters are larger than %d voxels.\n', length(clustSize), nClusters, k);
-        conCounter = conCounter + 7;
+        write_struct(outStruct, [outDir 'clusters_' sprintf('%03d', length(clustSize)) '.csv']);鈽㎙⟀騷
     else
         fprintf(['\tSkipping contrast ' num2str(iCon) ', ' SPM.xCon(iCon).name ', because it is an F con.\n']);
     end
 end
 
-% Write out summary csv.
-outMainDir = fileparts(outDir(1:end-1));
-if length(outStruct{1}.col) < maxClusters
-    outStruct{1}.col{maxClusters} = '';
-end
-
-write_struct(outStruct, [outMainDir '/clusters.csv']);
 fprintf('Done.\n\n');
 end
 
 %%
-function [peakCoord, peakValue, peakMM] = get_peak_d(niiFile, roiFile)
-%UNTITLED8 Summary of this function goes here
-%   Detailed explanation goes here
-
-V = spm_vol(niiFile);
-
-% Get ROI index and transform matrix.
-if exist(roiFile, 'file')
-    [XYZ ROImat] = roi_find_index(roiFile);
-else
-    error('Mask not found: %s\n', roiFile);
+function [peakValue, peakCoord, peakMM] = get_peak_d(dHeader, betaXYZ)
+% FORMAT [peakValue, peakCoord, peakMM] = get_peak_d(dHeader, betaXYZ)
+% Extracts coordinates, value, and location in mm of peak value in roi from
+% nifti image.
+% Extract values from ROI and get max and location in XYZ and coordinates.
+valuesInROI = spm_get_data(dHeader(1), betaXYZ{1});
+[peakValue, idx] = max(valuesInROI);
+maxLoc = betaXYZ{1}(:, idx)';
+peakCoord(1:3) = maxLoc * dHeader.mat(1:3, 1:3) + dHeader.mat(1:3, 4)'
+peakMM = maxLoc;
 end
 
-% Generate XYZ locations for each beta image correcting for alignment
-% issues and preallocate mean_roi vector.
-betaXYZ = adjust_XYZ(XYZ, ROImat, V);
+%% Extract Coordinates of ROI
+function [index, mat] = roi_find_index(ROI_loc, thresh)
+% FORMAT [index, mat] = roi_find_index(ROI_loc, thresh)
+% Returns the XYZ address of voxels with values greater than threshold. 
+% By Dennis Thompson.
+% 
+%
+% ROI_loc:          String pointing to nifti image.
+% thresh:           Threshold value, defaults to zero. Double.
 
-% Extract mean of ROI from each beta.
-betasInROI = spm_get_data(V(1), betaXYZ{1});
-[peakValue, idx] = max(betasInROI);
-maxLoc = betaXYZ{1}(:, idx);
-maxLocReal = [26 -60 -24];
-
-peakCoord = mm2vox(maxLoc, niiFile);
-peakMM = maxLoc.';
+if ~exist('thresh','var'),
+    thresh = 0;
 end
 
-%%
-function vox = mm2vox(maxLoc, niiFile)
-v2m = spm_get_space(niiFile);
-m2v = inv(v2m);
+data = nifti(ROI_loc);
+Y = double(data.dat);
+Y(isnan(Y)) = 0;
+index = [];
+for n = 1:size(Y, 3)
+    % find values greater > thresh
+    [xx, yy] = find(squeeze(Y(:, :, n)) > thresh);
+    if ~isempty(xx),
+        zz = ones(size(xx)) * n;
+        index = [index, [xx'; yy'; zz']];
+    end
+end
 
-vox(1:3) = maxLoc' * v2m(1:3, 1:3) + v2m(1:3, 4)';
+mat = data.mat;
+end
+
+%% Adjust Coordinates of ROI
+function funcXYZ = adjust_XYZ(XYZ, ROImat, V)
+% FORMAT funcXYZ = adjust_XYZ(XYZ, ROImat, V)
+% By Dennis Thompson.
+%
+%
+% XYZ:              Output from lss_roi_find_index.
+% ROImat:           Output from lss_roi_find_index.
+% V:                Header information of nifti file from spm_vol.
+
+XYZ(4, :) = 1;
+funcXYZ = cell(length(V));
+for n = 1:length(V),
+    if(iscell(V)),
+       tmp = inv(V{n}.mat) * (ROImat * XYZ);
+    else
+        tmp = inv(V(n).mat) * (ROImat * XYZ);
+    end
+    funcXYZ{n} = tmp(1:3, :);
+end
 end
