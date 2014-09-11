8ed57e95232c0ed06fefb2aac5d5f4d51ac80907
Taylor Salo <tsalo90@gmail.com>
2014-09-11 16:12:06 -0700

    save_clusters_and_effect_size now allows for cluster-extent based thresholding. It relies on spm_list_edited. batch_effect_size is an example script to call save_clusters_and_effect_size.

---
+++ batch_effect_size.m
@@ -0,0 +1,8 @@
+% Example wrapper to call save_clusters_and_effect_size.m function.
+spmFile = '/nfs/cntracs/analysis/MTC/func_an_SPM8_bestproc/070814_paired_t-test/HC/CueB-CueA/SPM.mat';
+
+pThr = {0.01 0.05};
+corr = {'unc' 'FWE'};
+k = 0;
+
+save_clusters_and_effect_size(spmFile, pThr, corr, k);
--- save_clusters_and_effect_size.m
+++ save_clusters_and_effect_size.m
@@ -8,8 +8,9 @@ function save_clusters_and_effect_size(spmFile, pThr, corr, k)
 %
 %
 % spmFile: Path to SPM.mat file (including SPM.mat). String.
-% pThr:    p threshold. Double.
-% corr:    Correction applied to p threshold (FWE, FDR, or unc). String.
+% pThr:    p threshold. Cell array (1x1 or 1x2) of doubles.
+% corr:    Correction applied to p threshold (FWE, FDR, or unc). Cell array
+%          (same length as pThr) of strings.
 % k:       Minimum acceptable cluster size. Double.
 
 if exist(spmFile, 'file')
@@ -19,43 +20,64 @@ else
     error('spmFile does not exist. Quitting.');
 end
 
-if ischar(pThr)
-    fprintf('Warning, your set p threshold is a string. Trying to convert to double.\n')
-    pThr = str2double(pThr);
-    if isnan(pThr)
-        fprintf('P threshold could not be converted to double, setting to default 0.001.\n');
-        pThr = 0.001;
-    end
-elseif ~isa(pThr, 'double')
-    fprintf('Warning, your set p threshold is not a double or a string. Setting to default 0.001.\n');
-    pThr = 0.001;
-end
-
-if ischar(k)
-    fprintf('Warning, your set k is a string. Trying to convert to double.\n')
-    k = str2double(k);
-    if isnan(k)
-        fprintf('P threshold could not be converted to double, setting to default 5.\n');
-        k = 5;
-    end
-elseif ~isa(k, 'double')
-    fprintf('Warning, your set k is not a double or a string. Setting to default 5.\n');
-    k = 5;
-end
+% if iscell(pThr)
+%     fprintf('Warning, your set p threshold is a string. Trying to convert to double.\n')
+%     pThr = str2double(pThr);
+%     if isnan(pThr)
+%         fprintf('P threshold could not be converted to double, setting to default 0.001.\n');
+%         pThr = 0.001;
+%     end
+% elseif ~isa(pThr, 'double')
+%     fprintf('Warning, your set p threshold is not a double or a string. Setting to default 0.001.\n');
+%     pThr = 0.001;
+% end
+% 
+% if ischar(k)
+%     fprintf('Warning, your set k is a string. Trying to convert to double.\n')
+%     k = str2double(k);
+%     if isnan(k)
+%         fprintf('P threshold could not be converted to double, setting to default 5.\n');
+%         k = 5;
+%     end
+% elseif ~isa(k, 'double')
+%     fprintf('Warning, your set k is not a double or a string. Setting to default 5.\n');
+%     k = 5;
+% end
 
 design = SPM.xsDes.Design;
 fprintf(['Evaluating second-level results of design: ' design '.\n']);
-fprintf(['Evaluating at p < ' num2str(pThr) ' ' corr ' and k > ' num2str(k) '.\n']);
+if length(pThr) == 1
+    fprintf(['Evaluating at p < ' num2str(pThr{1}) ' ' corr{1} ' and k > ' num2str(k) '.\n']);
+else
+    fprintf(['Evaluating at voxel-level p < ' num2str(pThr{1}) ' ' corr{1} ' and k > ' num2str(k) ' and cluster-level p < ' num2str(pThr{2}) ' ' corr{2} '.\n']);
+end
+rpvFile = [path '/RPV.img'];
 
 for iCon = 1:length(SPM.xCon)
-    STAT = SPM.xCon(iCon).STAT;
+    xSPM.STAT = SPM.xCon(iCon).STAT;
 
-    if strcmp(STAT, 'T')
+    if strcmp(xSPM.STAT, 'T')
+        xSPM.STAT = SPM.xCon(iCon).STAT;
+        xSPM.df = [SPM.xCon(iCon).eidf SPM.xX.erdf];
+        xSPM.k = k;
+        xSPM.VRpv = spm_vol(rpvFile);
+        xSPM.M = xSPM.VRpv.mat;
+        xSPM.n = 1;
+        xSPM.S = SPM.xVol.S;
+        xSPM.Ic = iCon;
+        xSPM.R = SPM.xVol.R;
+        xSPM.FWHM = SPM.xVol.FWHM;
+        
         spmT = [path '/' SPM.xCon(iCon).Vspm.fname];
         conName = ['Contrast_' sprintf('%03d', iCon) '-' strrep(SPM.xCon(iCon).name, ' ', '_')];
         fprintf(['\tEvaluating contrast ' num2str(iCon) ', ' conName '\n']);
-        outDir = [path '/' num2str(pThr) '_' corr '_clusters/' conName '/'];
-
+        
+        if length(pThr) == 1
+            outDir = [path '/v' num2str(pThr{1}) '_' corr{1} '_clusters/' conName '/'];
+        else
+            outDir = [path '/v' num2str(pThr{1}) '_' corr{1} '_c' num2str(pThr{2}) '_' corr{2} '_clusters/' conName '/'];
+        end
+        
         if ~exist(outDir, 'dir')
             mkdir(outDir);
         end
@@ -63,8 +85,7 @@ for iCon = 1:length(SPM.xCon)
         % Create Cohen's D image.
         VspmT = spm_vol(spmT);
         [Tvals, ~] = spm_read_vols(VspmT);
-        df = [SPM.xCon(iCon).eidf SPM.xX.erdf];
-        Dvals = (2 .* Tvals) ./ sqrt(df(2));
+        Dvals = (2 .* Tvals) ./ sqrt(xSPM.df(2));
         dHeader = VspmT;
         dHeader.fname = [outDir 'D_' conName '.nii'];
         outDHeader = spm_create_vol(dHeader);
@@ -73,32 +94,100 @@ for iCon = 1:length(SPM.xCon)
         % Determine voxel size of T image.
         [sizeX, sizeY, sizeZ] = get_voxel_size(spmT);
         voxelScalar = sizeX * sizeY * sizeZ;
-
+        xSPM.VOX = [sizeX sizeY sizeZ];
+        xSPM.units = {'mm' 'mm' 'mm'};
+        xSPM.DIM = SPM.xVol.DIM;
+        
         % Set up output csv.
         clear outStruct
-        outStruct{1}.header{1} = 'x'; outStruct{2}.header{1} = 'y'; outStruct{3}.header{1} = 'z';
-        outStruct{4}.header{1} = 'k'; outStruct{5}.header{1} = 'mm3'; outStruct{6}.header{1} = 'peak_T';
-        outStruct{7}.header{1} = 'peak_D'; outStruct{8}.header{1} = 'mean_D';
+        outStruct{1}.header{1} = 'Region of Activation'; outStruct{2}.header{1} = 'BA'; outStruct{3}.header{1} = 'L/R';
+        outStruct{4}.header{1} = 'k (mm3)'; outStruct{5}.header{1} = 'T'; outStruct{6}.header{1} = 'D';
+        outStruct{7}.header{1} = 'x'; outStruct{8}.header{1} = 'y'; outStruct{9}.header{1} = 'z';
         outStruct{1}.col{1} = ''; outStruct{2}.col{1} = ''; outStruct{3}.col{1} = '';
         outStruct{4}.col{1} = ''; outStruct{5}.col{1} = ''; outStruct{6}.col{1} = '';
-        outStruct{7}.col{1} = ''; outStruct{8}.col{1} = '';
+        outStruct{7}.col{1} = ''; outStruct{8}.col{1} = ''; outStruct{9}.col{1} = '';
 
         % Create masks of all significant clusters and determine mean
         % Cohen's D of each cluster.
-        switch corr
+        switch corr{1}
             case 'unc'
-                tThr = spm_u(pThr, df, STAT);
+                xSPM.u = spm_u(pThr{1}, xSPM.df, xSPM.STAT);
             case 'FWE'
-                tThr  = spm_uc(pThr, df, STAT, SPM.xVol.R, 1, SPM.xVol.S);
+                xSPM.u  = spm_uc(pThr{1}, xSPM.df, xSPM.STAT, xSPM.R, 1, xSPM.S);
             case 'FDR'
-                tThr  = spm_uc_FDR(pThr, df, STAT, 1, VspmT, 0);
+                xSPM.u  = spm_uc_FDR(pThr{1}, xSPM.df, xSPM.STAT, 1, VspmT, 0);
             otherwise
                 error('Variable corr must be either "unc", "FWE", or "FDR".');
         end
+        
+        [x, y, z] = ind2sub(size(Tvals), find(Tvals > xSPM.u));
+        XYZ = [x'; y'; z'];
+        Z = min(Inf, spm_get_data(SPM.xCon(iCon).Vspm, XYZ));
 
+        V2R = 1/prod(SPM.xVol.FWHM(SPM.xVol.DIM > 1));
+        [uc, xSPM.Pc, ue] = spm_uc_clusterFDR(0.05, xSPM.df, xSPM.STAT, xSPM.R, xSPM.n, Z, XYZ, V2R, xSPM.u);
+        [up, xSPM.Pp] = spm_uc_peakFDR(0.05, xSPM.df, xSPM.STAT, xSPM.R, xSPM.n, Z, XYZ, xSPM.u);
+
+        A = spm_clusters(XYZ);
+        Q = [];
+        for i = 1:max(A)
+            j = find(A == i);
+            if length(j) >= xSPM.k
+                Q = [Q j];
+            end
+        end
+
+        xSPM.Z     = Z(:,Q);
+        xSPM.XYZ   = XYZ(:,Q);
+        xSPM.XYZmm = xSPM.XYZ;
+
+        for jVox = 1:length(xSPM.XYZ)
+            xSPM.XYZmm(:, jVox) = (xSPM.XYZ(:, jVox).' * xSPM.M(1:3, 1:3) + xSPM.M(1:3, 4)').';
+        end
+        uu = spm_uc(0.05, xSPM.df, xSPM.STAT, xSPM.R, xSPM.n, xSPM.S);
+
+        xSPM.uc = [uu up ue uc];
+        
+        TabDat2 = spm_list_edited('List', xSPM);
+        table = TabDat2.dat(:, 3:end);
+
+        if length(corr) == 2
+            switch corr{2}
+                case 'unc'
+                    col = 4;
+                case 'FWE'
+                    col = 1;
+                case 'FDR'
+                    col = 2;
+            end
+        end
+
+        % Cluster row index.
+        clustLevel = table(:, 1);
+        clustIdx = find(~cellfun(@isempty, clustLevel));
+
+        % Pare down table
+        if length(pThr) == 2
+            for iClust = length(clustIdx):-1:1
+                if iClust == length(clustIdx)
+                    if table{clustIdx(iClust), col} > pThr{2}
+                        table(clustIdx(iClust):end, :) = [];
+                    end
+                else
+                    if table{clustIdx(iClust), col} > pThr{2}
+                        table(clustIdx(iClust):clustIdx(iClust + 1) - 1, :) = [];
+                    end
+                end
+            end
+        end
+        clustLevel = table(:, 1);
+        clustIdx = find(~cellfun(@isempty, clustLevel));
+                
+        clear x y z XYZ
+        
         clustHeader = VspmT;
         rawData = spm_read_vols(VspmT);
-        [allClustMat, nClusters] = spm_bwlabel(double(rawData > tThr), 18);
+        [allClustMat, nClusters] = spm_bwlabel(double(rawData > xSPM.u), 18);
 
         [clustSize, clustNum] = sort(histc(allClustMat(:), 0:nClusters), 1, 'descend');
         clustSize = clustSize(2:end); clustNum = clustNum(2:end) - 1;
@@ -112,23 +201,67 @@ for iCon = 1:length(SPM.xCon)
             betaXYZ = adjust_XYZ(XYZ, clustHeader.mat, dHeader);
             roi = spm_get_data(dHeader.fname, betaXYZ{1});
             roi(isnan(roi)) = 0;
-            [peakValue, peakCoord, peakMM] = get_peak_d(VspmT.fname, betaXYZ);
-            clustHeader.fname = [outDir 'Cluster_' sprintf('%03d', jClust) '_' num2str(peakCoord(1)) '_' num2str(peakCoord(2)) '_' num2str(peakCoord(3)) '.nii'];
-            spm_write_vol(clustHeader, oneClustMat);
+            clustNumber = 0;
+            for kClust = 1:length(clustIdx)
+                peakMM(1:3) = (table{clustIdx(kClust), 10}.' - VspmT.mat(1:3, 4)') / VspmT.mat(1:3, 1:3);
+                if sum(ismember(betaXYZ{1}.', peakMM, 'rows'))
+                    clustNumber = kClust;
+                    clustPeakMM = peakMM;
+                end
+            end
+            if clustNumber ~= 0
+                peakCoord = table{clustIdx(clustNumber), 10}.';
+                clustHeader.fname = [outDir 'Cluster_' sprintf('%03d', clustNumber) '_' num2str(peakCoord(1)) '_' num2str(peakCoord(2)) '_' num2str(peakCoord(3)) '.nii'];
+                spm_write_vol(clustHeader, oneClustMat);
+                
+                % Fill in output csv.
+                outStruct{1}.col{clustIdx(clustNumber), 1} = '';
+                outStruct{2}.col{clustIdx(clustNumber), 1} = '';
+                
+                
+                if table{clustIdx(clustNumber), 10}(1) < 0
+                    outStruct{3}.col{clustIdx(clustNumber), 1} = 'L';
+                elseif table{clustIdx(clustNumber), 10}(1) > 0
+                    outStruct{3}.col{clustIdx(clustNumber), 1} = 'R';
+                else
+                    outStruct{3}.col{clustIdx(clustNumber), 1} = 'I';
+                end
+                outStruct{4}.col{clustIdx(clustNumber), 1} = clustSize(jClust) * voxelScalar;
+                outStruct{5}.col{clustIdx(clustNumber), 1} = num2str(table{clustIdx(clustNumber), 7});
+                outStruct{6}.col{clustIdx(clustNumber), 1} = Dvals(clustPeakMM(1), clustPeakMM(2), clustPeakMM(3));
+                outStruct{7}.col{clustIdx(clustNumber), 1} = peakCoord(1);
+                outStruct{8}.col{clustIdx(clustNumber), 1} = peakCoord(2);
+                outStruct{9}.col{clustIdx(clustNumber), 1} = peakCoord(3);
+                
+                if clustNumber == length(clustIdx)
+                    [nRows, ~] = size(table);
+                else
+                    nRows = clustIdx(clustNumber + 1) - 1;
+                end
+                for mSubClust = clustIdx(clustNumber) + 1:nRows
+                    subPeakMM = (table{mSubClust, 10}.' - VspmT.mat(1:3, 4)') / VspmT.mat(1:3, 1:3);
 
-            % Fill in output csv.
-            outStruct{1}.col{jClust, 1} = peakCoord(1);
-            outStruct{2}.col{jClust, 1} = peakCoord(2);
-            outStruct{3}.col{jClust, 1} = peakCoord(3);
-            outStruct{4}.col{jClust, 1} = clustSize(jClust);
-            outStruct{5}.col{jClust, 1} = clustSize(jClust) * voxelScalar;
-            outStruct{6}.col{jClust, 1} = peakValue;
-            outStruct{7}.col{jClust, 1} = Dvals(peakMM(1), peakMM(2), peakMM(3));
-            outStruct{8}.col{jClust, 1} = sum(roi(:)) / length(find(roi));
+                    outStruct{1}.col{mSubClust, 1} = '';
+                    outStruct{2}.col{mSubClust, 1} = '';
+                    if table{mSubClust, 10}(1) < 0
+                        outStruct{3}.col{mSubClust, 1} = 'L';
+                    elseif table{mSubClust, 10}(1) > 0
+                        outStruct{3}.col{mSubClust, 1} = 'R';
+                    else
+                        outStruct{3}.col{mSubClust, 1} = 'I';
+                    end
+                    outStruct{4}.col{mSubClust, 1} = '';
+                    outStruct{5}.col{mSubClust, 1} = num2str(table{mSubClust, 7});
+                    outStruct{6}.col{mSubClust, 1} = Dvals(subPeakMM(1), subPeakMM(2), subPeakMM(3));
+                    outStruct{7}.col{mSubClust, 1} = table{mSubClust, 10}(1);
+                    outStruct{8}.col{mSubClust, 1} = table{mSubClust, 10}(2);
+                    outStruct{9}.col{mSubClust, 1} = table{mSubClust, 10}(3);
+                end
+            end
         end
 
-        fprintf('\t\t%d out of %d clusters are larger than %d voxels.\n', length(clustSize), nClusters, k);
-        write_csv(outStruct, [outDir 'ClusterReport_' sprintf('%03d', length(clustSize)) '.csv']);
+        fprintf('\t\t%d out of %d clusters are larger than %d voxels.\n', length(clustIdx), nClusters, k);
+        write_csv(outStruct, [outDir 'ClusterReport_' sprintf('%03d', length(clustIdx)) '.csv']);
     else
         fprintf(['\tSkipping contrast ' num2str(iCon) ', ' SPM.xCon(iCon).name ', because it is an F con.\n']);
     end
@@ -150,20 +283,6 @@ mniZ = (V.mat * [1 1 2 1]')';
 sizeX = pdist([mniO; mniX], 'euclidean');
 sizeY = pdist([mniO; mniY], 'euclidean');
 sizeZ = pdist([mniO; mniZ], 'euclidean');
-end
-
-%% Extract Value, MNI Coordinates, and Voxel Coordinates of Peak Value in ROI.
-function [peakValue, peakCoord, peakMM] = get_peak_d(niiFile, betaXYZ)
-% FORMAT [peakValue, peakCoord, peakMM] = get_peak_d(niiFile, betaXYZ)
-% Extracts coordinates, value, and location in mm of peak value in roi from
-% nifti image.
-% Extract values from ROI and get max and location in XYZ and coordinates.
-V = spm_vol(niiFile);
-valuesInROI = spm_get_data(V(1), betaXYZ{1});
-[peakValue, idx] = max(valuesInROI);
-maxLoc = betaXYZ{1}(:, idx)';
-peakCoord(1:3) = maxLoc * V.mat(1:3, 1:3) + V.mat(1:3, 4)';
-peakMM = uint8(maxLoc);
 end
 
 %% Adjust Coordinates of ROI
